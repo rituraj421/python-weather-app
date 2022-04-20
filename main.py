9b130c9824080297e757fd38dce2f2d16eeb9a76
rituraj <riturajroj1234@gmail.com>
2022-04-20 23:36:52 +0530

    main

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 from tkinter import *
 import tkinter as tk
+# from tkinter.tix import IMAGETEXT
 from geopy.geocoders import Nominatim
 from tkinter import ttk,messagebox
 from timezonefinder import TimezoneFinder
@@ -14,4 +15,13 @@ root.title("Weather App")
 root.geometry("900x500+300+200")
 root.resizable(False,False)
 
+
+#search bar
+Search_image=PhotoImage(file="media/search.png")
+myimage=Label(image=Search_image)
+myimage.place(x=20,y=20)
+
+textfield=tk.Entry(root,justify="center",width=17,font=("poppins",25,"bold"),bg="#404040",border=0,fg="white")
+textfield.place(x=50,y=40)
+textfield.focus()
 root.mainloop()
