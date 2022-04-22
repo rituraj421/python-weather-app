25b38f9639ef7c242414d199429c6c8954efe2aa
rituraj <riturajroj1234@gmail.com>
2022-04-22 23:13:43 +0530

    main

--- main.py
+++ main.py
@@ -15,6 +15,21 @@ root.title("Weather App")
 root.geometry("900x500+300+200")
 root.resizable(False,False)
 
+#22-04-22
+def getWeather():
+    city=textfield.get()
+
+    geolocator=Nominatim(user_agent="geoapiExercises")
+    location=geolocator.geocode(city)
+    obj = TimezoneFinder()
+    result=obj.timezone_at(lng=location.longitude,lat=location.latitude)
+
+    home=pytz.timezone(result) 
+    local_time=datetime.now(home)
+    current_time=local_time.strftime("%I:%M:%p")
+    clock.config(text=current_time)
+    name.config(text="CURRENT WEATRHER")
+    #end 22-04-22
 
 #search bar
 Search_image=PhotoImage(file="media/search.png")
@@ -28,7 +43,7 @@ textfield.focus()
 #21-04-2022 start
 
 Search_icon=PhotoImage(file="media/search_icon.png")
-myimage_icon=Button(image=Search_icon,borderwidth=0,cursor="hand2",bg="#404040")
+myimage_icon=Button(image=Search_icon,borderwidth=0,cursor="hand2",bg="#404040",command=getWeather)
 myimage_icon.place(x=400,y=34)
 
 #logo
@@ -40,6 +55,15 @@ logo.place(x=150,y=100)
 Frame_image=PhotoImage(file="media/box.png")
 frame_myimage=Label(image=Frame_image)
 frame_myimage.pack(padx=5,pady=5,side=BOTTOM)
+
+
+#22-04-22
+#time
+name=Label(root,font=("arial",15,"bold"))
+name.place(x=30,y=100)
+clock=Label(root,font=("Helvetica",20))
+clock.place(x=30,y=130)
+#end 22-04-22
 
 #label
 label1=Label(root,text="WIND",font=("Helvetica",15,'bold'),fg="white",bg="#1ab5ef")
@@ -62,5 +86,18 @@ c.place(x=400,y=250)
 w=Label(text="...",font=("arial",20,"bold"),bg="#1ab5ef")
 w.place(x=120,y=430)
 #21-04-2022 start
+
+#22-04-22
+w=Label(text="...",font=("arial",20,"bold"),bg="#1ab5ef")
+w.place(x=120,y=430)
+
+h=Label(text="...",font=("arial",20,"bold"),bg="#1ab5ef")
+h.place(x=280,y=430)
+
+d=Label(text="...",font=("arial",20,"bold"),bg="#1ab5ef")
+d.place(x=450,y=430)
+
+wp=Label(text="...",font=("arial",20,"bold"),bg="#1ab5ef")
+wp.place(x=670,y=430)
 
 root.mainloop()
